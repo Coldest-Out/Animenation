2e1f265175f11324de134fa864793ccc9668c986
Coldest-Out <98006784+Coldest-Out@users.noreply.github.com>
2023-05-04 17:16:21 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+//import './App.css'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<span>Home</span>
+  },
+  {
+    path:'/products/:id',
+    element:<span>Category</span>
+  },
+  {
+    path:'/product/:id',
+    element:<span>Product</span>
+  },
+])
+
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router}/>
+    </div>
+  );
+}
+
+export default App;
