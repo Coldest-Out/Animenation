5c02b4f1cb64e5aa59e3636d8002c4524b78f650
Coldest-Out <98006784+Coldest-Out@users.noreply.github.com>
2023-05-09 16:08:01 -0700

    created navbar & footer component skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,45 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home/Home';
+import Products from './pages/Products/Products';
 //import './App.css'
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
 
 const router = createBrowserRouter([
   {
-    path:'/',
-    element:<span>Home</span>
-  },
-  {
-    path:'/products/:id',
-    element:<span>Category</span>
-  },
-  {
-    path:'/product/:id',
-    element:<span>Product</span>
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/products/:id',
+        element: <Products />
+      },
+      {
+        path: '/product/:id',
+        element: <Products />
+      },
+    ]
   },
 ])
 
 function App() {
   return (
     <div>
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </div>
   );
 }
