203bdd2775487182640bc91e0bc6d50669dd9a0a
Coldest-Out <98006784+Coldest-Out@users.noreply.github.com>
2023-05-09 19:20:19 -0700

    navbar progress and styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home/Home';
 import Products from './pages/Products/Products';
-//import './App.css'
+import './App.scss';
 
 const Layout = () => {
   return (
