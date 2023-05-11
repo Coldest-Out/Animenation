ece7b62e633239b22c7a6020c19ace75d2aa7c25
Coldest-Out <98006784+Coldest-Out@users.noreply.github.com>
2023-05-10 22:09:56 -0700

    fixed react array error

--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import Card from "../Card/Card";
 import './FeaturedProducts.scss';
 
-const FeaturedProducts = (type) => {
+const FeaturedProducts = ({type}) => {
 
 	const data = [
 		{
@@ -31,24 +32,27 @@ const FeaturedProducts = (type) => {
 			price: 12,
 		},
 		{
-			id: 3,
+			id: 4,
 			img: "../../../public/img/anime-hat1.jpg",
 			img2: "../../../public/img/anime-hat2.jpg",
 			title: "Anime Hats",
 			oldPrice: 19,
 			price: 12,
-		}
-	]
+		},
+	];
 
 
 	return (
-		<div className="FeaturedProducts">
+		<div className="featuredProducts">
 			<div className="top">
+				{/* <h1>{data.map (type => <div>{data.type}</div>)} products</h1> */}
 				<h1>{type} products</h1>
 				<p>Our amazing anime brand clothing is a testament to our passion for Japanese animation, with each design capturing the essence of beloved characters and iconic moments. The attention to detail and quality craftsmanship of our apparel showcases our commitment to providing fans with a truly immersive and authentic experience. From vibrant, intricately embroidered jackets to stylish graphic tees that evoke nostalgia, our anime brand clothing is a perfect fusion of fashion and fandom, making it a must-have for anime enthusiasts everywhere.</p>
 			</div>
 			<div className="bottom">
-
+				{data.map(item => (
+					<Card item={item} key={item.id} />
+				))}
 			</div>
 		</div>
 	);
