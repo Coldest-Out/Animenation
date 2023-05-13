02deb1b0b5588805062930e14b9eac11af069dac
Coldest-Out <98006784+Coldest-Out@users.noreply.github.com>
2023-05-12 22:41:36 -0700

    categories component and styling

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,26 +1,57 @@
 import React from 'react';
 import './Categories.scss';
+import { Link } from 'react-router-dom';
+import AnimeCat1 from '../../../public/img/anime-cat1.jpeg';
+import AnimeCat2 from '../../../public/img/anime-cat2.png';
+import AnimeCat3 from '../../../public/img/anime-cat3.jpeg';
+import AnimeCat4 from '../../../public/img/anime-cat4.jpeg';
+import AnimeCat5 from '../../../public/img/anime-cat5.png';
+import AnimeCat6 from '../../../public/img/anime-cat6.jpeg';
 
 const Categories = () => {
 	return (
 		<div className="categories">
 			<div className="col">
-				<div className="row">r1</div>
-				<div className="row">r2</div>
+				<div className="row">
+					<img src={AnimeCat1} alt="" />
+					<button>
+						<Link className='link' to='/products/1'>Sale</Link>
+					</button>
+				</div>
+				<div className="row">
+				<img src={AnimeCat2} alt="" />
+					<button>
+						<Link className='link' to='/products/1'>Women</Link>
+					</button>
+				</div>
 			</div>
 			<div className="col">
-				<div className="row">r3</div>
+				<div className="row">
+				<img src={AnimeCat3} alt="" />
+					<button>
+						<Link className='link' to='/products/1'>New Season</Link>
+					</button>
+				</div>
 			</div>
 			<div className="col col-l">
 				<div className="row">
 					<div className="col">
-						<div className="row">r4</div>
+						<div className="row"><img src={AnimeCat4} alt="" />
+					<button>
+						<Link className='link' to='/products/1'>Men</Link>
+					</button></div>
 					</div>
 					<div className="col">
-						<div className="row">r5</div>
+						<div className="row"><img src={AnimeCat5} alt="" />
+					<button>
+						<Link className='link' to='/products/1'>Accessories</Link>
+					</button></div>
 					</div>
 				</div>
-				<div className="row">r6</div>
+				<div className="row"><img src={AnimeCat6} alt="" />
+					<button>
+						<Link className='link' to='/products/1'>Shoes</Link>
+					</button></div>
 			</div>
 		</div>
 	)
