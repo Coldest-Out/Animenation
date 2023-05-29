655d679368dbc0674315b46234bc523c386077ec
Coldest-Out <98006784+Coldest-Out@users.noreply.github.com>
2023-05-28 21:57:01 -0700

    Update List.jsx

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,52 +1,22 @@
 import React from 'react';
 import './List.scss';
 import Card from '../Card/Card';
+import useFetch from '../../hooks/useFetch';
 
-const List = () => {
+const List = ({ subCats, maxPrice, sort, catId }) => {
 
-	const data = [
-		{
-			id: 1,
-			img: "../../../public/img/anime-longsleeve1.jpg",
-			img2: "../../../public/img/anime-longsleeve1.jpg",
-			title: "Anime T-Shirt",
-			isNew: true,
-			oldPrice: 19,
-			price: 12,
-		},
-		{
-			id: 2,
-			img: "../../../public/img/anime-coat1.jpg",
-			img2: "../../../public/img/anime-coat2.jpg",
-			title: "Anime Coat",
-			isNew: true,
-			oldPrice: 19,
-			price: 12,
-		},
-		{
-			id: 3,
-			img: "../../../public/img/anime-pants1.jpg",
-			img2: "../../../public/img/anime-pants2.jpg",
-			title: "Anime Pants",
-			oldPrice: 19,
-			price: 12,
-		},
-		{
-			id: 4,
-			img: "../../../public/img/anime-hat1.jpg",
-			img2: "../../../public/img/anime-hat2.jpg",
-			title: "Anime Hats",
-			oldPrice: 19,
-			price: 12,
-		},
-	];
+	const { data, loading, error } = useFetch(
+    `/products?populate=*&[filter][categories][id]=${catId}${subCats.map(
+      (item) => `&[filters][sub_categories][id][$eq]=${item}`
+    )}`
+  );
 
 	return (
 		<div className="list">
-			{/* {data?.map(item => (
-			<Card item={item} key={item.id} />
-		))} */}
-		</div>
+      {loading
+        ? "loading"
+        : data?.map((item) => <Card item={item} key={item.id} />)}
+    </div>
 	);
 };
 
