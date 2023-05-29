6368f6065dcbe7628cd2868fa7e57d2a7cb38094
Coldest-Out <98006784+Coldest-Out@users.noreply.github.com>
2023-05-28 21:57:23 -0700

    Update Product.jsx

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -8,38 +8,46 @@ import Outfit2 from '/img/outfit2.png';
 import Outfit3 from '/img/outfit3.png';
 import Outfit4 from '/img/outfit4.png';
 import Outfit5 from '/img/outfit5.png';
+import useFetch from '../../hooks/useFetch';
+import { useParams } from "react-router-dom";
 
 
 const Product = () => {
-	const [selectedImage, setSelectedImg] = useState(0);
+	const id = useParams().id;
+	const [selectedImage, setSelectedImg] = useState("img");
 	const [quantity, setQuantity] = useState(1);
 
-	const images = [
-		Outfit1,
-		Outfit2,
-		Outfit3,
-		Outfit4,
-		Outfit5,
-	]
+	// const images = [
+	// 	Outfit1,
+	// 	Outfit2,
+	// 	Outfit3,
+	// 	Outfit4,
+	// 	Outfit5,
+	// ]
+
+	const { data, loading, error } = useFetch(`/products/${id}?populate=*`);
+	console.log(data);
 
 	return(
 		<div className='product'>
-			<div className="left">
+			{ loading ? "loading" : ( <><div className="left">
 				<div className="images">
-					<img src={images[0]} alt="" onClick={e => setSelectedImg(0)}/>
-					<img src={images[1]} alt="" onClick={e => setSelectedImg(1)}/>
-					<img src={images[2]} alt="" onClick={e => setSelectedImg(2)}/>
-					<img src={images[3]} alt="" onClick={e => setSelectedImg(3)}/>
-					<img src={images[4]} alt="" onClick={e => setSelectedImg(4)}/>
+					<img src={"http://localhost:5678" + data?.attributes?.img?.data?.attributes?.url} alt="" onClick={e => setSelectedImg("img")}/>
+					<img src={"http://localhost:5678" + data?.attributes?.img2?.data?.attributes?.url} alt="" onClick={e => setSelectedImg("img2")}/>
+					{/* <img src={"http://localhost:5678" + data?.attributes?.img?.data?.attributes?.url} alt="" onClick={e => setSelectedImg(2)}/>
+					<img src={"http://localhost:5678" + data?.attributes?.img?.data?.attributes?.url} alt="" onClick={e => setSelectedImg(3)}/>
+					<img src={"http://localhost:5678" + data?.attributes?.img?.data?.attributes?.url} alt="" onClick={e => setSelectedImg(4)}/> */}
+					{/* <img src={images[4]} alt="" onClick={e => setSelectedImg(4)}/> */}
 				</div>
 				<div className="mainImg">
-					<img src={images[selectedImage]} alt="" />
+					<img src={"http://localhost:5678" + data?.attributes[selectedImage]?.data?.attributes?.url} alt="" />
+					{/* <img src={images[selectedImage]} alt="" /> */}
 				</div>
 			</div>
 			<div className="right">
-				<h1>Title</h1>
-				<span className='price'>$19.99</span>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa est totam itaque cupiditate quae! Deserunt nam quae maiores suscipit expedita ut eos, vel ullam voluptas!</p>
+				<h1>{data?.attributes?.title}</h1>
+				<span className='price'>{data?.attributes?.price}</span>
+				<p>{data?.attributes?.desc}</p>
 				<div className="quantity">
 					<button onClick={() => setQuantity((prev) => prev ===1 ? 1 : prev-1)}>-</button>
 					{quantity}
@@ -70,7 +78,8 @@ const Product = () => {
 					<span>FAQ</span>
 				</div>
 
-			</div>
+			</div></>
+			)}
 		</div>
 	)
 }
